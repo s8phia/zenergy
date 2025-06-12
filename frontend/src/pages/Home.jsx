f57c653ef29627f46e5cd5f55582492e903a12b4
Sophia <sophiawg143@gmail.com>
2025-06-11 21:28:52 -0400

    functionality for to do list added- ui needed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,12 +5,13 @@ import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
   const [username, setUsername] = useState('')
-
   const[tasks, setTasks] = useState([])
   const [title, setTitle] = useState('')
   const [description, setDescription] = useState('')
   const [energy, setEnergy] = useState('')
   const [showPopup, setShowPopup] = useState(false)
+  const [isEditMode, setIsEditMode] = useState(false)
+  const [editTaskId, setEditTaskId] = useState(null)
 
   const navigate = useNavigate()
   const fetchUser = async () => {
@@ -43,32 +44,72 @@ const Home = () => {
       console.error('Error fetching tasks:', err)
     }
   }
-  
-  const handleAddTask = async () => {
+
+  const openAddPopup = () => {
+    setTitle('')
+    setDescription('')
+    setEnergy('')
+    setIsEditMode(false)
+    setShowPopup(true)
+  }
+
+  const openEditPopup = (task) => {
+    setTitle(task.title)
+    setDescription(task.description)
+    setEnergy(task.energy)
+    setEditTaskId(task._id)
+    setIsEditMode(true)
+    setShowPopup(true)
+  }
+
+  const handleSubmit = async () => {
+    const token = localStorage.getItem('token')
+    const url = isEditMode
+      ? `http://localhost:5000/tasks/${editTaskId}`
+      : 'http://localhost:5000/tasks'
+
+    const method = isEditMode ? 'put' : 'post'
     try {
-      const token = localStorage.getItem('token')
-      const response = await axios.post('http://localhost:5000/tasks', {
-          title,
-          description,
-          energy
-        }, {
-          headers: { Authorization: `Bearer ${token}` }
-        })
-      if (response.status === 201) {
-        setTitle('')
-        setDescription('')
-        setEnergy('')
-        fetchTasks()
+      await axios[method](url, {
+        title, 
+        description,
+        energy_level: energy
+      },
+      {
+        headers: { Authorization: `Bearer ${token}` }
       }
-      
-    } catch (err) {
-      console.error('Error adding task:', err)
+    )
+
+    fetchTasks()
+    setShowPopup(false)
+    setTitle('')
+    setDescription('')
+    setEnergy('')
+    setIsEditMode(false)
+    setEditTaskId(null)
+
+    }catch(err){
+      console.error('Error submitting task:', err)
     }
   }
+
+  const handleDelete = async (id) => {
+    try{
+      const token = localStorage.getItem('token')
+      await axios.delete(`http://localhost:5000/tasks/${id}`, {
+        headers: { Authorization: `Bearer ${token}` }
+      })
+      fetchTasks()
+    } catch (err){
+      console.error('Error deleting task:', err)
+    }
+  }
+
   useEffect(() => {
     fetchUser()
     fetchTasks()
   }, [])
+  
   return (
     <div>
       <div>
